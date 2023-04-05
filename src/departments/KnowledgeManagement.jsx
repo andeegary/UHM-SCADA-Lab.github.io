74742a96de02cdbb340c0ebb8c8602254ef2a718
Eliya Nakamura <eliyanak@hawaii.edu>
2023-04-04 16:31:22 -1000

    Updates to KM documentation

--- src/departments/KnowledgeManagement.jsx
+++ src/departments/KnowledgeManagement.jsx
@@ -89,12 +89,16 @@ export default App;
     </p>
     <pre>{'   <Link to="/PATH">Link to Page</Link>'}</pre>
     <p>which will appear as <Link to="/PATH">Link to Page</Link>.</p>
-    <h3 className="pt-2">Previewing a Page in React (under construction)</h3>
+    <h3 className="pt-2">Previewing a Page in React</h3>
     <p>Before you proceed, you will need to install Node.js, which is an open source server environment that we will use to preview and deploy our website. You can download the latest installer for your device <Link to="https://nodejs.org/en/download">here</Link>.</p>
     <p>When you want to preview a website page on your local device for the first time, you will need to open up a new terminal window within the repository folder. You can do this within Intellij by going to <em>View &gt; Tool Windows &gt; Terminal</em> or by pressing <em>Alt + F12</em>.</p>
     <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/terminal.png" alt="How to open terminal" />
     <p className="pt-2">Alternatively, you can open the folder within Git Bash or your program of choice.</p>
-    <p>You will then have to run <code>npm install</code> to install any relevant files needed to run the website&apos;s React application.</p>
+    <p>You will then have to run <code>npm install</code> to install any relevant files needed to run the website&apos;s React application. If the command is successful, you should see a message similar to this:</p>
+    <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/npm_install.PNG" alt="npm install" />
+    <p className="pt-2">Now that all the necessary files have been installed, you only have to run <code>npm start</code> to compile and start up the application. It should look similar to the image below.</p>
+    <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/npm_start.png" alt="npm start" />
+    <p className="pt-2">You should now see a live preview of your website! You should either see a new tab open in your browser that displays the application, or you can visit the URL listed in the console (In the case of the example screenshot above, the URL is <code>http://localhost:3000</code>).</p>
     <h3 className="pt-2">React Bootstrap Tips</h3>
     <p>Important note: Borders and titles are added for visualization purposes.</p>
     <h4>Grid Layout</h4>
