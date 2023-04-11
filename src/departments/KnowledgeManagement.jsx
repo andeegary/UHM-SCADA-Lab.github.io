833d1fac30ad765e9db737cea5e6d82a4ac90d9e
Eliya Nakamura <eliyanak@hawaii.edu>
2023-04-10 14:49:54 -1000

    KM documentation update

--- src/departments/KnowledgeManagement.jsx
+++ src/departments/KnowledgeManagement.jsx
@@ -95,12 +95,12 @@ export default App;
     <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/terminal.png" alt="How to open terminal" />
     <p className="pt-2">Alternatively, you can open the folder within Git Bash or your program of choice.</p>
     <p>You will then have to run <code>npm install</code> to install any relevant files needed to run the website&apos;s React application. If the command is successful, you should see a message similar to this:</p>
-    <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/npm_install.PNG" alt="npm install" />
+    <img style={{ width: '100%', maxWidth: '600px' }} src="/images/KnowledgeManagement/npm_install.PNG" alt="npm install" />
     <p className="pt-2">Now that all the necessary files have been installed, run <code>npm start</code> to compile and start up the application. It should look similar to the image below.</p>
     <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/npm_start.png" alt="npm start" />
     <p className="pt-2">You should now see a live preview of your website! You should either see a new tab open in your browser that displays the application, or you can visit the URL listed in the console (in the case of the example below, the URL is <code>http://localhost:3000</code>).</p>
     <img style={{ width: '100%', maxWidth: '800px' }} src="/images/KnowledgeManagement/react_preview.PNG" alt="React preview" />
-    <h3 className="pt-2">React Bootstrap Tips</h3>
+    <h3 className="pt-4">React Bootstrap Tips</h3>
     <p>Important note: Borders and titles are added for visualization purposes.</p>
     <h4>Grid Layout</h4>
     <Row className="justify-content-center pb-3">
@@ -206,6 +206,8 @@ export default App;
     <hr />
     <h2>Administrator Guides</h2>
     <h3 className="pt-3">Publishing New Changes</h3>
+    <p>After any updates have been pushed to the repository, Github Pages also needs to be updated by the administrator so that the changes are visible on the website. To do this, run <code>npm run deploy</code> in the terminal. <em>Don&apos;t forget that Node.js should be installed on your device and that </em><code>npm install</code> <em>should have already been run!</em></p>
+    <img style={{ width: '100%', maxWidth: '600px' }} src="/images/KnowledgeManagement/npm_deploy.PNG" alt="Output when deploying website" />
   </Container>
 );
 
