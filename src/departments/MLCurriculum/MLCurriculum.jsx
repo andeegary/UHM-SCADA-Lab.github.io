27b5f74573638a796c6be955d27c98b4ad52c020
jebryce <116392465+jebryce@users.noreply.github.com>
2023-04-18 14:50:37 -1000

    Updated overall goal of ML team

--- src/departments/MLCurriculum/MLCurriculum.jsx
+++ src/departments/MLCurriculum/MLCurriculum.jsx
@@ -5,10 +5,13 @@ import ZeroTrust from './ZeroTrust';
 const MLCurriculum = () => (
   <Container className="py-3">
     <h1>Machine Learning Curriculum</h1>
-    <h2>Current Tasks</h2>
+    <h2>Overall Goal</h2>
     <p>
-      None as of 9/8<br />
-      John needs to get his act together and start working on this...
+      Very vaguely, we want to use some machine learning technique to interact with our lab&apos;s SDN (Software Defined Networking) controller to allow or deny network traffic flows.<br />
+      <br />
+      This software defined network is implemented through a protocol called Openflow, which can be used to create software-based network components, such as routers, firewalls, and load-balancers. We want to use SDN to create a firewall that doesn&apos;t use the traditional rules- or policy-based control, but rather some machine learning algorithm that can allow or deny network flow requests.<br />
+      <br />
+      These network flow requests come from a physical network switch, that has been configured to interact with an SDN controller, which, in our setup, is simply a Raspberry Pi running a Python script. <br />
     </p>
     <Tabs defaultActiveKey="zeroTrust">
       <Tab eventKey="zeroTrust" title="Zero Trust" className="py-4">
