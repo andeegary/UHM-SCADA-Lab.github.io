14253fab49b53ce4be83b4ae7d76e8be8b974f6b
jebryce <116392465+jebryce@users.noreply.github.com>
2023-04-18 15:00:54 -1000

    moved overallGoal into it's own tab similar to zeroTrust

--- src/departments/MLCurriculum/MLCurriculum.jsx
+++ src/departments/MLCurriculum/MLCurriculum.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Container, Tab, Tabs } from 'react-bootstrap';
 import ZeroTrust from './ZeroTrust';
+import OverallGoal from './OverallGoal';
 
 const MLCurriculum = () => (
   <Container className="py-3">
     <h1>Machine Learning Curriculum</h1>
-    <h2>Overall Goal</h2>
-    <p>
-      Very vaguely, we want to use some machine learning technique to interact with our lab&apos;s SDN (Software Defined Networking) controller to allow or deny network traffic flows.<br />
-      <br />
-      This software defined network is implemented through a protocol called Openflow, which can be used to create software-based network components, such as routers, firewalls, and load-balancers. We want to use SDN to create a firewall that doesn&apos;t use the traditional rules- or policy-based control, but rather some machine learning algorithm that can allow or deny network flow requests.<br />
-      <br />
-      These network flow requests come from a physical network switch, that has been configured to interact with an SDN controller, which, in our setup, is simply a Raspberry Pi running a Python script. <br />
-    </p>
-    <Tabs defaultActiveKey="zeroTrust">
+    <Tabs defaultActiveKey="overallGoal">
+      <Tab eventKey="overallGoal" title="Overall Goal" className="py-4">
+        <OverallGoal />
+      </Tab>
       <Tab eventKey="zeroTrust" title="Zero Trust" className="py-4">
         <ZeroTrust />
       </Tab>
